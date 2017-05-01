ce9419fb969afb8076538099e92ec7ac98caec6b
Martin R. Vasilev <martin.r.vasilev@gmail.com>
2017-05-01 12:08:14 +0100

    Update parse_da1.R

--- R/parse_da1.R
+++ R/parse_da1.R
@@ -152,8 +152,10 @@ parse_da1<- function(list_da1= "C:/Users/Martin Vasilev/Documents/Cdiff_data/dat
           out$word[i]<- curr_word
         }
         
-        if(i>1 & out$X6[i-1]=="."){
+        if(i>1){
+          if(& out$X6[i-1]=="."){
           curr_word<-1
+          }
         }
 
       }
